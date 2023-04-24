ceb903580181de22e857eab030b935858ac6a63f
adetimmy <ogunleademola@gmail.com>
2023-04-24 06:42:14 +0100

    added finacial and color-mapping components

--- src/pages/Charts/Bar.tsx
+++ src/pages/Charts/Bar.tsx
@@ -1,39 +1,37 @@
 import { ColumnSeries, Tooltip, DataLabel, Highlight, BarSeries, Category, Legend, ChartComponent, Inject, SeriesCollectionDirective, SeriesDirective } from'@syncfusion/ej2-react-charts'
 import { Header } from '../../component'
 import { barCustomSeries } from '../../data/dummy'
-import { useStateContext } from '../../context/ContentProvider'
-
-
 
 const Bar = () => {
-  const { currentMode } = useStateContext()
+
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category='Chart' title=''/>
-      <ChartComponent
-        id='charts'
-        style={{backgroundColor:'red'}}
-        chartArea={{border: {width:0}}}
-        tooltip={{enable:true}}
-        background={currentMode === 'Dark'? '#33373E' : '#fff' }
-        legendSettings={{enableHighlight:true}}
-        primaryXAxis={{  'None' : 'Trim', valueType: 'Category', interval: 1, majorGridLines: { width: 0 }, majorTickLines: { width: 0 }, edgeLabelPlacement: 'Shift' }}
-        primaryYAxis={{
-          title: 'Medal Count',
-          majorTickLines: { width: 0 }, lineStyle: { width: 0 }, maximum: 50, interval: 10,
-      }} 
-      >
-        <Inject 
-          services={[BarSeries, Category, Legend, ColumnSeries, Tooltip, DataLabel, Highlight]}
-        />
-        <SeriesCollectionDirective>
-          {barCustomSeries.map(
-            (item, index) => (
-              <SeriesDirective key={index} {...item} />
-            ) 
-          )}
-        </SeriesCollectionDirective>
-      </ChartComponent>
+      <Header category='Chart' title='Bar'/>
+      <div className='w-full'>
+        <ChartComponent
+          id='charts'
+          title='Olympic Medal Counts - RIO'
+          chartArea={{border: {width:0}}}
+          tooltip={{enable:true}}
+          legendSettings={{enableHighlight:true, background: 'white'}}
+          primaryXAxis={{   valueType: 'Category', interval: 1, majorGridLines: { width: 0 }, majorTickLines: { width: 0 }, edgeLabelPlacement: 'Shift' }}
+          primaryYAxis={{
+            majorTickLines: { width: 0 }, lineStyle: { width: 0 }, maximum: 50, interval: 10,
+        }} 
+        >
+          <Inject 
+            services={[BarSeries, Category, Legend, ColumnSeries, Tooltip, DataLabel, Highlight]}
+          />
+          <SeriesCollectionDirective>
+            {barCustomSeries.map(
+              (item, index) => (
+                <SeriesDirective key={index} {...item} />
+              ) 
+            )}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+      
     </div>
   )
 }
--- src/pages/Charts/ColorMapping.tsx
+++ src/pages/Charts/ColorMapping.tsx
@@ -1,8 +1,51 @@
 
-const ColorMapping = () => {
-  return (
-    <div>ColorManager</div>
-  )
-}
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Category, Tooltip, Legend, RangeColorSettingsDirective, RangeColorSettingDirective } from '@syncfusion/ej2-react-charts';
 
-export default ColorMapping
+import { colorMappingData, ColorMappingPrimaryXAxis, ColorMappingPrimaryYAxis, rangeColorMapping } from '../../data/dummy';
+import { Header } from '../../component';
+import { useStateContext } from '../../context/ContentProvider';
+
+const ColorMapping = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Color Mappping" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          title='USA CLIMATE - WEATHER BY MONTH'
+          primaryXAxis={ColorMappingPrimaryXAxis}
+          primaryYAxis={ColorMappingPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          legendSettings={{ mode: 'Range', background: 'white' }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={colorMappingData[0]}
+              name="USA"
+              xName="x"
+              yName="y"
+              type="Column"
+              cornerRadius={{
+                topLeft: 10,
+                topRight: 10,
+              }}
+            />
+          </SeriesCollectionDirective>
+          <RangeColorSettingsDirective>
+      
+            {rangeColorMapping.map((item, index) => <RangeColorSettingDirective key={index} {...item}>
+              </RangeColorSettingDirective>
+              )}
+          </RangeColorSettingsDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default ColorMapping;
