7d02d38ba7b1866c5693970f1ebe23dda0ccdeff
adetimmy <ogunleademola@gmail.com>
2023-05-12 06:39:01 +0100

    done done done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
@@ -73,9 +73,13 @@ export const App = () => {
                 <Route path='/stacked' element={<Stacked/>}/>
               </Routes>
             </div>
+            <Footer/>
         </div>
-        </div>
+       
+        </div> 
+        
       </BrowserRouter>  
+      
     </div>
   )
 }
